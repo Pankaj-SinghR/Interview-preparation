c5b69abc8353eca81b4f7b8ab8a941fe1fac7813
Pankaj Singh <pankajsingh132000@gmail.com>
2025-11-17 09:58:15 +0530

    factorial of a numb

--- dsa/recursion/recursion.js
+++ dsa/recursion/recursion.js
@@ -1,8 +1,17 @@
-function printNumber(n) {
-  if (n === 0) return
-  printNumber(n - 1)
-  console.log(n)
+// function printNumber(n) {
+//   if (n === 0) return
+//   printNumber(n - 1)
+//   console.log(n)
+// }
+//
+// printNumber(5)
+
+// 5*4*
+function factorialNumber(n) {
+  if (n === 1) return 1
+  return n * factorialNumber(n - 1)
 }
 
-printNumber(5)
+const res = factorialNumber(5)
+console.log(res)
 
