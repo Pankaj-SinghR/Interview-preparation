04e7a50d53ed3055976157d2f519e053b4e87d5f
Pankaj Singh <pankajsingh132000@gmail.com>
2025-11-18 10:25:36 +0530

    factorial of a numb

--- dsa/recursion/recursion.js
+++ dsa/recursion/recursion.js
@@ -6,12 +6,22 @@
 //
 // printNumber(5)
 
-// 5*4*
+// factorial of a number
 function factorialNumber(n) {
-  if (n === 1) return 1
+  if (n <= 1) return 1
   return n * factorialNumber(n - 1)
 }
 
-const res = factorialNumber(5)
-console.log(res)
+// const res = factorialNumber(5)
+// console.log(res)
+
+// sum of digits
+function sumOfDigits(n) {
+  if (n <= 0) return 0
+  const rem = n % 10
+  const num = parseInt(n / 10)
+  return rem + sumOfDigits(num)
+}
+
+console.log(sumOfDigits(54321))
 
