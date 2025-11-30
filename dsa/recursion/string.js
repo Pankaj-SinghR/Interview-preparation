49eef698763d94de86dcbe37770c18473d41d0bf
Pankaj Singh <pankajsingh132000@gmail.com>
2025-11-30 12:08:44 +0530

    pattern

---
+++ dsa/recursion/string.js
@@ -0,0 +1,16 @@
+
+function skip(str, val) {
+  let ans = ""
+  ans = skipHelperFun(str, i = 0, val, ans)
+  return ans
+}
+
+function skipHelperFun(str, i, val, ans) {
+  if (str.length === i + 1) return ans
+  if (str[i] !== val) ans += str[i]
+  return skipHelperFun(str, i + 1, val, ans)
+}
+
+const res = skip("bbaaccdd", "a")
+console.log(res)
+
