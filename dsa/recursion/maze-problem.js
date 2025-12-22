a52d618c31f0d5f65da2f44f61a334f74dd29ae4
Pankaj Singh <pankajsingh132000@gmail.com>
2025-12-22 10:19:27 +0530

    chors: path count is right and down is allowed

--- dsa/recursion/maze-problem.js
+++ dsa/recursion/maze-problem.js
@@ -1,3 +1,14 @@
 // maze problem
 
+function pathCount(r, c) {
+  if (r === 1 || c === 1) {
+    return 1
+  }
+  const left = pathCount(r - 1, c)
+  const right = pathCount(r, c - 1)
+  return left + right
+}
+
+const res = pathCount(2, 3)
+console.log(res)
 
