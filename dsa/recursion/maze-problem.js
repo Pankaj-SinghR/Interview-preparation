251cbfbaf0cf0f6fae0d50d9e4b05d0c22a2adfe
Pankaj Singh <pankajsingh132000@gmail.com>
2025-12-22 10:58:58 +0530

    feat: return array for pathPrint

--- dsa/recursion/maze-problem.js
+++ dsa/recursion/maze-problem.js
@@ -9,6 +9,36 @@ function pathCount(r, c) {
   return left + right
 }
 
-const res = pathCount(2, 3)
+// const res = pathCount(2, 3)
+// console.log(res)
+
+function pathPrint(path, r, c) {
+  let ans = []
+  if (r === 1 && c === 1) {
+    ans.push(path)
+    return ans
+  }
+
+  if (r === 1 && c > 1) {
+    const rr = pathPrint(path + "R", r, c - 1)
+    ans = [...ans, ...rr]
+  }
+
+  if (c === 1 && r > 1) {
+    const cc = pathPrint(path + "D", r - 1, c)
+    ans = [...ans, ...cc]
+  }
+
+  if (r > 1 && c > 1) {
+    const rr = pathPrint(path + "D", r - 1, c)
+    ans = [...ans, ...rr]
+    const cc = pathPrint(path + "R", r, c - 1)
+    ans = [...ans, ...cc]
+  }
+
+  return ans
+}
+
+const res = pathPrint("", 3, 3)
 console.log(res)
 
