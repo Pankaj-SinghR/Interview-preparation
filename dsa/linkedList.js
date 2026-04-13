64ca786b3a62d20e67560743ac235c3ccb44df02
Pankaj Singh <pankajsingh132000@gmail.com>
2026-04-13 23:10:48 +0530

    chors: merged a linked list

--- dsa/linkedList.js
+++ dsa/linkedList.js
@@ -49,4 +49,44 @@ var hasCycle = function(head) {
     }
     return false
 };
+// ----------------------------------------------------------- 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    // create a new node and insert linked list node init
+    let tempNode = new ListNode(0, null)
+    let mergedNode = tempNode
+    let curr1 = list1
+    let curr2 = list2
+    while(curr1 !== null && curr2 !== null){
+        if(curr1.val < curr2.val){
+            mergedNode.next = curr1
+            mergedNode = curr1
+            curr1 = curr1.next
+        }else {
+            mergedNode.next = curr2
+            mergedNode = curr2
+            curr2 = curr2.next
+        }
+    }
+    // merge the remaning elements,
+    // NOTE: It's if and else not while loop
+    if(curr1 !== null){
+        mergedNode.next = curr1
+    }
+    if(curr2 !== null){
+        mergedNode.next = curr2
+    }
+    return tempNode.next
+};
 
