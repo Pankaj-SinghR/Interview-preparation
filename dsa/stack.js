8b0e63530f1b7b1a365794e9645e1903577465e2
Pankaj Singh <pankajsingh132000@gmail.com>
2026-04-16 12:53:28 +0530

    chors: minStack using 2 stack
    main stack and min stack

--- dsa/stack.js
+++ dsa/stack.js
@@ -29,3 +29,58 @@ var isValid = function (s) {
     return stack.length ? false:true
 };
 
+// ---------------------------------------------------------------------------------- // 
+
+var MinStack = function() {
+    this.mainStack = []
+    this.minStack = []
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.mainStack.push(val)
+    // check if minStack is already empty
+    if(!this.minStack.length){
+        this.minStack.push(val)
+    } else if(val <= this.minStack[this.minStack.length-1] && this.minStack.length){
+    // if val < top of minStack then only push it
+        this.minStack.push(val)
+        // console.log(this.minStack)
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    const ele = this.mainStack.pop()
+    if(ele === this.minStack[this.minStack.length-1]){
+        this.minStack.pop()
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.mainStack[this.mainStack.length-1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length-1]
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ *
