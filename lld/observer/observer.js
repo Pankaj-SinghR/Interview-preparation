8862fd9b02b58a4e282834b907a73b55ebdf08c4
Pankaj Singh <pankajsingh132000@gmail.com>
2026-05-11 18:32:37 +0530

    chors: minor changes

--- lld/observer/observer.js
+++ lld/observer/observer.js
@@ -1,3 +1,46 @@
 // observer pattern 
 // if one thing changes, all the other things that depend 
 // on it will be notified and updated automatically
+
+class EmailNotify {
+    update(order) {
+        console.log("Email sent for order", order.id)
+    }
+}
+
+class SMSNotify {
+    update(order) {
+        console.log("SMS sent for order", order.id)
+    }
+}
+
+class Order {
+    constructor() {
+        this.observers = []
+    }
+
+    subscribe(observer) {
+        this.observers.push(observer)
+    }
+
+    // private function
+    sentNotification(order) {
+        for (let ob of this.observers) {
+            ob.update(order)
+        }
+    }
+
+    placeOrder(order) {
+        console.log("Order has been placed: ", order.id)
+        this.sentNotification(order)
+    }
+}
+
+const orderVal = {
+    id: "12313"
+}
+
+const or = new Order()
+or.subscribe(new EmailNotify())
+or.subscribe(new SMSNotify())
+or.placeOrder(orderVal)
